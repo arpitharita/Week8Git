a4c0442225cd39586cd2620b1512160e52c7c2c0
arpitharita <arpit17785@gmail.com>
2023-11-05 03:46:43 -0500

    week 8

--- Week8_Homewotk/src/question25/Question25.java
+++ Week8_Homewotk/src/question25/Question25.java
@@ -3,4 +3,15 @@ package question25;
 //Declare one method with return type and parameter and print your name in console.
 
 public class Question25 {
+    public static void main(String[] args) {
+        String Name = "Harita";
+        String reversedStr = "";
+        System.out.println(" Name : " + Name);
+
+        for (int i = 0; i < Name.length(); i++) {
+            reversedStr = Name.charAt(i) + reversedStr;
+        }
+
+        System.out.println("Reversed string: " + reversedStr);
+    }
 }
--- Week8_Homewotk/src/question3/Question3.java
+++ Week8_Homewotk/src/question3/Question3.java
@@ -7,9 +7,17 @@ public class Question3 {
 
     public static void main(String[] args) {
 
-        String[][] Country = {{"London,Canada,Usa,India,Australia"},
-                             {"Ontario,Harrow,Florida,Delhi,Melbourne"}};
-          System.out.println(Country.length);
+        Question3 que = new Question3();
+        que.CountryCapital();
 
+    }
+    public void CountryCapital(){
+        String[][] CounrtyCapital= {{"London","Harrow"},{"Canada,Ontario"},{"Usa,Newyork"},{"India,Delhi"},{"Australia,Melbourne"}};
+
+        for (int i =0; i < CounrtyCapital.length; i++){
+            String Country= CounrtyCapital[0][1];
+            String Capital = CounrtyCapital[i][1];
+            System.out.println("Country : " +Country + " , Capital " +Capital);
+        }
     }
 }
